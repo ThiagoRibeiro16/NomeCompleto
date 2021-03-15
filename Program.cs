9f0aa3d7bb941b797e80287395957a6fff31efee
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-14 23:05:27 -0300

    Finalização do Programa

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string Nome;
+            string Sobrenome;
+            String NomeCompleto;
+            string nomeCatalogo;
+            
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Veja seu nome em diferentes perpectivas");
+            Console.WriteLine("---------------------------------------");
+            
+            Console.Write("Digite seu nome:");
+            Nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome:");
+            Sobrenome = Console.ReadLine();
+
+            NomeCompleto = $"{Nome} {Sobrenome}";
+            nomeCatalogo = $"{Sobrenome.ToUpper()} , {Nome}";
+
+            Console.WriteLine($"Nome completo: {NomeCompleto}");
+            Console.WriteLine($"Nome de Catálogo: {nomeCatalogo}");
+
+
+
+        }
+    }
+}
